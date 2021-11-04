d00710e825f2ff9ade8e159d1b683f5046753e7a
kristof22122 <kristf44@gmail.com>
2021-11-04 22:09:08 +0200

    Rervuews widget: setStar method deleted

--- reviews-widget/src/components/Star/Star.js
+++ reviews-widget/src/components/Star/Star.js
@@ -2,32 +2,26 @@ import React from 'react';
 import StarSCSS from './Star.module.scss';
 
 export class Star extends React.Component {
-  setStar = (starNumber, handleChange) => {
-    return (
-      <label
-        htmlFor={`star-${starNumber}`}
-        title={`Star ${starNumber}`}
-        className={StarSCSS.rating__label}
-        key={starNumber}
-      >
-        <input
-          type="radio"
-          id={`star-${starNumber}`}
-          name="rating"
-          value={starNumber}
-          className={StarSCSS.rating__item}
-          onChange={handleChange}
-          checked={false}
-        />  
-      </label>
-    );
-  };
-
   render() {
     const { handleChange, star } = this.props;
     return (
       <>
-        {this.setStar(star, handleChange)}        
+        <label
+          htmlFor={`star-${star}`}
+          title={`Star ${star}`}
+          className={StarSCSS.rating__label}
+          key={star}
+        >
+          <input
+            type="radio"
+            id={`star-${star}`}
+            name="rating"
+            value={star}
+            className={StarSCSS.rating__item}
+            onChange={handleChange}
+            checked={false}
+          />
+      </label>
       </>
     );
   }
